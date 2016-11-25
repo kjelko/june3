b2d9d8331e1ce6a74e0d5aa51819d1816bde7d6b
Kevin Elko <kjelko@gmail.com>
2016-11-25 10:49:00 -0500

    model updates

--- app/models.py
+++ app/models.py
@@ -1,1 +1,23 @@
 from google.appengine.ext import ndb
+
+
+class RsvpStatus(object):
+  NO_RESPONSE = 'no_response'
+  COMING = 'coming'
+  NOT_COMING = 'not_coming'
+
+
+class FoodChoice(ndb.Model):
+  name = ndb.StringProperty()
+  description = ndb.StringProperty()
+
+
+class Guest(ndb.Model):
+  name = ndb.StringProperty()
+  food_choice = ndb.KeyProperty(kind=FoodChoice)
+
+
+class Invitation(ndb.Model):
+  code = ndb.StringProperty()
+  guests = ndb.KeyProperty(kind=Guest, repeated=True)
+  rsvp = ndb.StringProperty(default=RsvpStatus.NO_RESPONSE)
