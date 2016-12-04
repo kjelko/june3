675c453eb07f059f25a0bcf0c4b9179b233a2444
Kevin Elko <kjelko@gmail.com>
2016-12-04 16:53:56 -0500

    admin site updates

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -33,21 +33,7 @@ PageController.prototype.$onInit = function() {
   this.http_.get('/api/food_choice').then(function(resp) {
     this.foodChoices = resp.data;
   }.bind(this));
-
-  //this.renderCaptcha();
 };
-
-
-// PageController.prototype.renderCaptcha = function() {
-//   if (!this.window_.grecaptcha) {
-//     setTimeout(this.renderCaptcha.bind(this), 10);
-//     return;
-//   }
-//   this.window_.grecaptcha.render(document.getElementById(this.recaptchaId), {
-//     callback: this.recaptchaSuccess_.bind(this),
-//     sitekey: this.recaptchaKey
-//   });
-// }
 
 
 PageController.prototype.updateShowArrow_ = function() {
@@ -125,6 +111,96 @@ PageController.prototype.handleError_ = function(resp) {
 };
 
 
+AdminController = function ($rootScope, $http, $mdToast) {
+  this.rootScope_ = $rootScope;
+  
+  this.http_ = $http;
+
+  this.invitations = [];
+
+  this.guests = [];
+
+  this.isLoading = true;
+
+  this.foodChoices = [];
+
+  this.mdToast = $mdToast;
+
+  this.stats = {
+    numGuests: 0,
+    numResponded: 0,
+    numAttending: 0,
+    foodChoices: {}
+  };
+};
+
+
+AdminController.prototype.$onInit = function() {
+  this.http_.get('/admin/api/invitation').then(this.parseInivitations_.bind(this));
+
+  this.http_.get('/api/food_choice').then(function(resp) {
+    this.foodChoices = resp.data || [];
+  }.bind(this));
+};
+
+
+AdminController.prototype.parseInivitations_ = function(resp) {
+  this.invitations = resp.data;
+  for (var i = 0; i < this.invitations.length; i++) {
+    for (var j = 0; j < this.invitations[i].guests.length; j++) {
+      var guest = this.invitations[i].guests[j];
+      this.guests.push(guest);
+      if (guest.rsvp == 'coming') {
+        this.stats.numAttending++;
+        this.stats.numResponded++;
+        if (guest.food_choice) {
+          this.foodChoices[guest.food_choice.name] = 
+              this.foodChoices[guest.food_choice.name] || 0;
+          this.foodChoices[guest.food_choice.name]++;
+        } 
+      } else if (guest.rsvp == 'not_coming') {
+        this.stats.numResponded++;
+      }
+    }
+  }
+  this.stats.numGuests = this.guests.length;
+  this.isLoading = false;
+}
+
+
+AdminController.prototype.updateFoodChoice = function(foodChoice) {
+  this.http_.post('/admin/api/food_choice', foodChoice).then(function() {
+    this.mdToast.shoSimple('Updated');
+  }.bind(this));
+};
+
+
+AdminController.prototype.createFoodChoice = function(event) {
+  var foodChoice = event.target.elements;
+  this.http_.post('/admin/api/food_choice', {
+    'name': foodChoice.name.value,
+    'description': foodChoice.description.value,
+  }).then(function(resp) {
+    foodChoice.name.value = '';
+    foodChoice.description.value = '';
+    this.foodChoices.push(resp.data);
+    this.mdToast.showSimple('Created');
+  }.bind(this));
+};
+
+
+AdminController.prototype.deleteFoodChoice = function(foodChoice) {
+  this.http_.delete('/admin/api/food_choice', {params: foodChoice}).then(function(resp) {
+    for (var i = 0; i < this.foodChoices.length; i++) {
+      if (this.foodChoices[i].id == foodChoice.id) {
+        this.foodChoices.splice(i, 1);
+      }
+    }
+    this.mdToast.showSimple('Deleted');
+  }.bind(this));
+}
+
+
 var config = function($mdThemingProvider) {
   $mdThemingProvider.definePalette('june3Primary', 
     $mdThemingProvider.extendPalette('green', {
@@ -144,5 +220,6 @@ var config = function($mdThemingProvider) {
 
 angular.module('June3App', ['ngMaterial', 'duScroll'])
     .controller('PageController', ['$window', '$rootScope', '$http', PageController])
+    .controller('AdminController', ['$rootScope', '$http', '$mdToast', AdminController])
     .config(['$mdThemingProvider', config])
     .value('duScrollOffset', 54);
