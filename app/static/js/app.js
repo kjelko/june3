a005869aac9296875e4c7d398b32cd76d76cf943
Kevin Elko <kjelko@gmail.com>
2016-12-05 23:27:34 -0500

    swap hero pic

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,9 @@
 angular.module('June3App', ['ngMaterial', 'duScroll'])
-    .controller('PageController', ['$window', '$rootScope', '$http', PageController])
-    .controller('AdminController', ['$rootScope', '$http', '$mdToast', AdminController])
-    .config(['$mdThemingProvider', config])
-    .value('duScrollOffset', 54);
+  .controller('PageController', [
+        '$window', '$rootScope', '$http', PageController
+  ])
+  .controller('AdminController', [
+        '$rootScope', '$http', '$mdToast', AdminController
+  ])
+  .config(['$mdThemingProvider', config])
+  .value('duScrollOffset', 54);
